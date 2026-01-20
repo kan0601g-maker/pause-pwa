d8a7e46324d60140974d88a7067293c3f0ac9dbe
kan0601g-maker <kan0601g@gmail.com>
2026-01-20 21:04:15 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>PAUSE</h1>
+      <p>気楽にいていい場所</p>
+
+      <p>
+        書いてもいいし、<br />
+        書かなくてもいい。<br />
+        無理に前向きにならなくても大丈夫です。
+      </p>
+    </main>
+  );
+}
