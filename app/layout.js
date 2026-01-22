deacd04b0a5a9cf27b46f03a8a3cc9faca18086b
kan0601g-maker <kan0601g@gmail.com>
2026-01-23 02:45:43 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
+export const metadata = {
+  title: "pause-pwa",
+  description: "pause-pwa",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
     </html>
   );
 }
