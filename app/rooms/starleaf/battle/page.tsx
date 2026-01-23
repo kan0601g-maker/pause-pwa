32028ef06fea79679840537aa54517fbe9dd5527
kan0601g-maker <kan0601g@gmail.com>
2026-01-24 03:40:04 +0900

    Delete app/rooms/starleaf/battle/page.tsx

--- app/rooms/starleaf/battle/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function StarleafBattlePage() {
-  return (
-    <div style={{ minHeight: "100vh", background: "#050a07", color: "#dfffea", padding: 24 }}>
-      <h1>STAR LEAF / BATTLE</h1>
-      <p>battle page ok</p>
-    </div>
-  );
-}
