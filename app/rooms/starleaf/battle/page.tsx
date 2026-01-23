e817d0ad7878bdca010db2ec191d37e810143ef0
kan0601g-maker <kan0601g@gmail.com>
2026-01-24 03:35:08 +0900

    Update page.tsx

--- app/rooms/starleaf/battle/page.tsx
+++ app/rooms/starleaf/battle/page.tsx
@@ -1,8 +1,1 @@
-export default function StarleafBattlePage() {
-  return (
-    <div style={{ minHeight: "100vh", background: "#050a07", color: "#dfffea", padding: 24 }}>
-      <h1>STAR LEAF / BATTLE</h1>
-      <p>battle page ok</p>
-    </div>
-  );
-}
+page.jsx
