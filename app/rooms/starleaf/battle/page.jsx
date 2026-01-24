8aa48661a8cd4d3f0acc5569b3a0de748ec885df
kan0601g-maker <kan0601g@gmail.com>
2026-01-24 14:58:08 +0900

    Delete app directory

--- app/rooms/starleaf/battle/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function BattlePage() {
-  return (
-    <main style={{ minHeight: "100vh", padding: 24 }}>
-      <h1>STAR LEAF / BATTLE</h1>
-      <p>battle page alive ✅</p>
-    </main>
-  );
-}
--- app/rooms/starleaf/tactics/page.js
+++
@@ -1,6 +0,0 @@
-// app/rooms/starleaf/tactics/page.js
-import TacticsClient from "./tactics-client.jsx";
-
-export default function Page() {
-  return <TacticsClient />;
-}
