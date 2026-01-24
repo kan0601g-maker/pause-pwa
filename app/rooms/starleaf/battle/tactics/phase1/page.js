e4cf30478961734b61452d7eabc7ce32e7c45e3f
kan0601g-maker <kan0601g@gmail.com>
2026-01-24 10:07:08 +0900

    Update page.js

--- app/rooms/starleaf/battle/tactics/phase1/page.js
+++ app/rooms/starleaf/battle/tactics/phase1/page.js
@@ -1,10 +1,1 @@
-"use client";
-
-export default function Phase1() {
-  return (
-    <div style={{ minHeight: "100vh", padding: 24 }}>
-      <h1>STARLEAF / TACTICS (Phase1)</h1>
-      <p>ここから Phase1 実装を積む</p>
-    </div>
-  );
-}
+export default function Page(){return <div>Phase1 alive ✅</div>}
